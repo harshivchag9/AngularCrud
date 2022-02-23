ab0c873c2ed73aaf7152ccf0b672b896875cc077
harshivchag9 <63035103+harshivchag9@users.noreply.github.com>
2022-02-23 18:05:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.listen(process.env.PORT || 8080);
 //PATH LOCATION STARTEGY
 
 app.get('/*', function (req, res) {
-    const fullPath = path.join(__dirname + '/dist/AngularCrud/index.html');
+    const fullPath = path.join(__dirname + '/dist/angular-crud/index.html');
     console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 })
