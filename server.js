a626785b9baf42e5144f4d6a07dcd28aedfc4f33
Harsh Patel <harshpatel4905@gmail.com>
2022-02-23 17:14:27 +0530

    chnge2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.listen(process.env.PORT || 8080);
 //PATH LOCATION STARTEGY
 
 app.get('/*', function (req, res) {
-const fullPath = path.join(__dirname + '/dist/AngularCrud/index.html');
+const fullPath = path.join(__dirname + '/dist/AngularCrud/src/index.html');
 console.log(" Fetching from.." + fullPath);
 res.sendFile(fullPath);
 })
