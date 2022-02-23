c29644cae79af428310a7a9615befb5089249c16
harshivchag9 <63035103+harshivchag9@users.noreply.github.com>
2022-02-23 18:00:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json'); // <== Will be created later
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3000; // <== You can change the port
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
-
 const express = require('express');
-
+const app = express();
 const path = require('path');
 
-const app = express();
-
-app.use(express.static('./dist/ANGULARCRUD'));
-
+app.use(express.static(__dirname + '/dist/AngularCrud'));
+app.listen(process.env.PORT || 8080);
 
 //PATH LOCATION STARTEGY
 
-app.get('/*', (req, res) =>
-res.sendFile('index.html', {root: 'dist/ANGULARCRUD'}),
-);
+app.get('/*', function (req, res) {
+    const fullPath = path.join(__dirname + '/dist/AngularCrud/index.html');
+    console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
 
-
-app.listen(process.env.PORT || 8080);
+console.log('Server started running..');
